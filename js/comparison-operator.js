783712648bd2b8f6a228741a79db344e041cfc59
Bijaya Manandhar <manandhar.bijaya@gmail.com>
2017-03-27 13:51:22 -0700

    first commit

---
+++ js/comparison-operator.js
@@ -0,0 +1,7 @@
+// 
+var pass = 50;    // Pass mark
+var score = 90;   // score
+
+// Check if the user has passed
+var el - document.getElementById('answer');
+el.TextContent = 'Level passed: ' + hasPassed;
