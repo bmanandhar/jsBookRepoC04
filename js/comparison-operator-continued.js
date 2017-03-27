0adce072c403d3395cf1e221ee824fa2229eaf4d
Bijaya Manandhar <manandhar.bijaya@gmail.com>
2017-03-27 13:57:45 -0700

    added a new file js/comparison-operator-continued.js

---
+++ js/comparison-operator-continued.js
@@ -0,0 +1,9 @@
+var score1 = 90;     // Round 1 score
+var score2 = 95;     // Round 2 score
+var highScore1 = 75; // Round 1 high score
+var highScore2 = 95; // Round 2 high score
+
+// Check if scores are higher than current high scores
+var comparison.getElementById('answer');
+el.textContent = 'New high score: ' + comparison;
+//
